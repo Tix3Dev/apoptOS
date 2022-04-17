c7a6bf28e3a0a557746f739c5e69f1a1483f6d2c
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-04-18 01:57:27 +0200

    [new] add COM driver

---
+++ src/kernel/devices/com.c
@@ -0,0 +1,84 @@
+/*
+	This file is part of a modern x86_64 UNIX-like microkernel which is called apoptOS
+	Everything is openly developed on GitHub: https://github.com/Tix3Dev/apoptOS
+
+	Copyright (C) 2022  Yves Vollmeier <https://github.com/Tix3Dev>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+
+Brief file description:
+Provides basic buffer based communication via COM.
+
+*/
+
+#include <stdbool.h>
+
+#include <assembly/utils.h>
+#include <devices/com.h>
+
+/* utility function prototypes */
+
+bool com_is_transmit_empty(com_port_t port);
+bool com_is_received(com_port_t port);
+
+/* core functions */
+
+// set basic values for a given COM port
+void com_init(com_port_t port)
+{
+    io_outb(port + 1, 0x00);
+    io_outb(port + 3, 0x80);
+    io_outb(port + 0, 0x03);
+    io_outb(port + 1, 0x00);
+    io_outb(port + 3, 0x03);
+    io_outb(port + 2, 0xC7);
+    io_outb(port + 4, 0x0B);
+}
+
+// send data (char) to a given COM port
+void com_send_char(com_port_t port, char c)
+{
+    while (!com_is_transmit_empty(port));
+
+    io_outb(port, c);
+}
+
+// send data (string) to a given COM port
+void com_send_string(com_port_t port, char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+	com_send_char(port, str[i]);
+}
+
+// get information from a given COM port
+char com_recv(com_port_t port)
+{
+    while (!com_is_received(port));
+
+    return io_inb(port);
+}
+
+/* utility functions */
+
+// check if transmission buffer of a given COM port is empty
+bool com_is_transmit_empty(com_port_t port)
+{
+    return io_inb(port + 5) & 0x20;
+}
+
+// check if transmission buffer of a given COM port is not empty
+bool com_is_received(com_port_t port)
+{
+    return io_inb(port + 5) & 1;
+}
---
+++ src/kernel/devices/com.h
@@ -0,0 +1,34 @@
+/*
+	This file is part of a modern x86_64 UNIX-like microkernel which is called apoptOS
+	Everything is openly developed on GitHub: https://github.com/Tix3Dev/apoptOS
+
+	Copyright (C) 2022  Yves Vollmeier <https://github.com/Tix3Dev>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef COM_H
+#define COM_H
+
+typedef enum
+{
+    COM1 = 0x3F8,
+    COM2 = 0x2F8,
+    COM3 = 0x3E8,
+    COM4 = 0x2E8
+} com_port_t;
+
+void com_init(com_port_t port);
+void com_send_char(com_port_t port, char c);
+void com_send_string(com_port_t port, char *str);
+char com_recv(com_port_t port);
+
+#endif
