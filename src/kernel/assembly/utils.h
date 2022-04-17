f6e217a31cbc068da1cbb37163d506f1a949499a
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-04-18 01:56:16 +0200

    [new] add outb & inb

---
+++ src/kernel/assembly/utils.h
@@ -0,0 +1,27 @@
+/*
+	This file is part of a modern x86_64 UNIX-like microkernel which is called apoptOS
+	Everything is openly developed on GitHub: https://github.com/Tix3Dev/apoptOS
+
+	Copyright (C) 2022  Yves Vollmeier <https://github.com/Tix3Dev>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+void io_outb(uint16_t port, uint8_t value);
+uint8_t io_inb(uint16_t port);
+void io_wait(void);
+
+#endif
