989c57bdfac91f13239ec4e006e40839f08001bb
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-04-23 14:36:49 +0200

    [new] local changes push

---
+++ src/kernel/memory/pmm.c
@@ -0,0 +1,42 @@
+/*
+	This file is part of a modern x86_64 UNIX-like microkernel which is called apoptOS
+	Everything is openly developed on GitHub: https://github.com/Tix3Dev/apoptOS
+
+	Copyright (C) 2022  Yves Vollmeier <https://github.com/Tix3Dev>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+
+    Brief file description:
+
+*/
+
+#include <boot/stivale2.h>
+#include <boot/stivale2_boot.h>
+#include <memory/pmm.h>
+
+/* utility function prototypes */
+
+// 
+
+/* core functions */
+
+void pmm_init(struct stivale2_struct *stivale2_struct)
+{
+    struct stivale2_struct_tag_memmap *memory_map = stivale2_get_tag(stivale2_struct,
+	    STIVALE2_STRUCT_TAG_MEMMAP_ID);
+}
+
+/* utility functions */
+
+//
