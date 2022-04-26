5ab1459c5a87f8f0090144ca3f7ed71687c2e0b6
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-04-26 12:52:03 +0200

    [new] working PMM

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -26,17 +26,78 @@
 #include <stdint.h>
 
 #include <boot/stivale2.h>
+#include <libk/serial/debug.h>
 #include <libk/serial/log.h>
+#include <memory/pmm.h>
 #include <tables/gdt.h>
 #include <tables/idt.h>
+
+/* utility function prototypes */
+
+void kinit_all(struct stivale2_struct *stivale2_struct);
+
+/* core functions */
 
 void kmain(struct stivale2_struct *stivale2_struct)
 {
     log(INFO, "Kernel started\n");
+    
+    kinit_all(stivale2_struct);
 
-    gdt_init();
-    idt_init();
+
+    debug_set_color(TERM_PURPLE);
+
+    void *ptr1 = pmm_alloc(1);
+    debug("1: ptr1 allocated at: 0x%x\n", ptr1);
+
+    void *ptr2 = pmm_alloc(1);
+    debug("1: ptr2 allocated at: 0x%x\n", ptr2);
+
+    void *ptr3 = pmm_alloc(1);
+    debug("1: ptr3 allocated at: 0x%x\n", ptr3);
+
+    void *ptr4 = pmm_alloc(2);
+    debug("2: ptr4 allocated at: 0x%x\n", ptr4);
+
+    void *ptr5 = pmm_alloc(3);
+    debug("3: ptr5 allocated at: 0x%x\n", ptr5);
+
+    void *ptr6 = pmm_alloc(8);
+    debug("8: ptr6 allocated at: 0x%x\n", ptr6);
+
+    void *ptr7 = pmm_alloc(1);
+    debug("1: ptr7 allocated at: 0x%x\n", ptr7);
+
+    pmm_free(ptr1, 1);
+    pmm_free(ptr2, 1);
+    pmm_free(ptr3, 1);
+    pmm_free(ptr4, 2);
+    pmm_free(ptr5, 3);
+    pmm_free(ptr6, 8);
+    pmm_free(ptr7, 7);
+
+    void *ptr8 = pmm_alloc(1);
+    debug("1: ptr8 allocated at: 0x%x\n", ptr8);
+
+    debug_set_color(TERM_PURPLE);
+
+    for (;;)
+    {
+	void *ptr9 = pmm_alloc(10000);
+	debug("1: ptr9 allocated at: 0x%x\n", ptr9);
+    }
+
+    log(INFO, "All kernels parts initialized\n");
 
     for (;;)
         asm volatile("hlt");
+}
+
+/* utility functions */
+
+void kinit_all(struct stivale2_struct *stivale2_struct)
+{
+    gdt_init();
+    idt_init();
+    pmm_init(stivale2_struct);
 }
--- src/kernel/memory/pmm.c
+++ src/kernel/memory/pmm.c
@@ -18,24 +18,33 @@
 /*
 
     Brief file description:
-    Physical memory management through bitmap based allocator.
+    Physical memory management through bitmap based allocator. Basic concept:
+    Each bit in the bitmap corresponds to a page (block of memory) through a
+    mapping system. A bit only says if the corresponding page is free or used.
 
 */
 
 #include <boot/stivale2.h>
 #include <boot/stivale2_boot.h>
+#include <libk/data_structs/bitmap.h>
+#include <libk/serial/debug.h>
+#include <libk/serial/log.h>
+#include <libk/string/string.h>
+#include <memory/mem.h>
 #include <memory/pmm.h>
 
 bitmap_t pmm_bitmap;
-static uint32_t used_pages = 0;
+static size_t highest_page_top = 0;
+static size_t used_pages_count = 0;
 
 /* utility function prototypes */
 
-const char *get_memory_map_entry_type(uint32_t type);
-void *pmm_find_first_free_page(size_t page_count);
+const char *get_memmap_entry_type_string(uint32_t type);
+void *pmm_find_first_free_page_range(size_t page_count);
 
 /* core functions */
 
+// handle memory map passed by stivale2 and host bitmap for allocator
 void pmm_init(struct stivale2_struct *stivale2_struct)
 {
     struct stivale2_struct_tag_memmap *memory_map = stivale2_get_tag(stivale2_struct,
@@ -43,17 +52,18 @@ void pmm_init(struct stivale2_struct *stivale2_struct)
     struct stivale2_mmap_entry *current_entry;
 
     size_t current_page_top = 0;
-    size_t highest_page_top = 0;
+
+    /* handle memory map passed by stivale2 */
     
     log(INFO, "Memory map layout:\n");
 
     for (uint64_t i = 0; i < memory_map->entries; i++)
     {
-	current_entry = memory_map->memmap[i];
+	current_entry = &memory_map->memmap[i];
 
 	debug_set_color(TERM_PURPLE);
         debug("Memory map entry No. %.16d: Base: 0x%.16llx | Length: 0x%.16llx | Type: %s\n",
-              i, current_entry->base, current_entry->length, get_memory_map_entry_type(current_entry->type));
+              i, current_entry->base, current_entry->length, get_memmap_entry_type_string(current_entry->type));
 	debug_set_color(TERM_COLOR_RESET);
 
 	if (current_entry->type != STIVALE2_MMAP_USABLE &&
@@ -64,21 +74,14 @@ void pmm_init(struct stivale2_struct *stivale2_struct)
 	current_page_top = current_entry->base + current_entry->length;
 
 	if (current_page_top > highest_page_top)
-	    highest_page = current_page_top;
+	    highest_page_top = current_page_top;
     }
 
-    used_pages = KB_TO_PAGES(highest_page_top);
+    used_pages_count = KB_TO_PAGES(highest_page_top);
 
-    pmm_bitmap.size = ALIGN_UP(ALIGN_DOWN(highest_page, PAGE_SIZE) / PAGE_SIZE / 8, PAGE_SIZE);
+    pmm_bitmap.size = ALIGN_UP(ALIGN_DOWN(highest_page_top, PAGE_SIZE) / PAGE_SIZE / 8, PAGE_SIZE);
 
-    log(INFO, "Memory specifications:\n");
-
-    debug_set_color(TERM_PURPLE);
-    debug("current_entry->base = %d\n", current_entry->base);
-    debug("current_entry->length - 1 = %d\n", current_entry->length);
-    debug_set_color(TERM_COLOR_RESET);
-    // debug("Total amount of memory: %d KB\n", );
-    // deubg("Size of PMM bitmap: %d KB\n", );
+    /* host bitmap for allocator */
 
     for (uint64_t i = 0; i < memory_map->entries; i++)
     {
@@ -119,87 +122,80 @@ void pmm_init(struct stivale2_struct *stivale2_struct)
     log(INFO, "PMM initialized\n");
 }
 
+// set free memory range to used and return base pointer
 void *pmm_alloc(size_t page_count)
 {
-    if (used_pages <= 0)
+    if (used_pages_count <= 0)
         return NULL;
 
-    void *pointer = pmm_find_first_free_page(page_count);
+    void *pointer = pmm_find_first_free_page_range(page_count);
 
     if (pointer == NULL)
         return NULL;
-
 
     uint64_t index = (uint64_t)pointer / PAGE_SIZE;
 
     for (size_t i = 0; i < page_count; i++)
         bitmap_set_bit(&pmm_bitmap, index + i);
 
-    used_pages += page_count;
+    used_pages_count += page_count;
 
-    return (void *)(uint64_t)(phys_to_higher_half_data(index * PAGE_SIZE));
+    return (void *)BIT_TO_PAGE(index);
 }
 
+// set status of n pages to unused
 void pmm_free(void *pointer, size_t page_count)
 {
-    uint64_t index = higher_half_data_to_phys((uint64_t)pointer) / PAGE_SIZE;
+    uint64_t index = PAGE_TO_BIT(pointer);
 
     for (size_t i = 0; i < page_count; i++)
         bitmap_unset_bit(&pmm_bitmap, index + i);
 
-    used_pages -= page_count;
+    used_pages_count -= page_count;
 }
 
 /* utility functions */
 
-const char *get_memory_map_entry_type(uint32_t type)
+// convert a stivale2 memory map entry type to a string
+const char *get_memmap_entry_type_string(uint32_t type)
 {
     switch (type)
     {
         case STIVALE2_MMAP_USABLE:
             return "Usable";
-
         case STIVALE2_MMAP_RESERVED:
             return "Reserved";
-
         case STIVALE2_MMAP_ACPI_RECLAIMABLE:
             return "ACPI Reclaimable";
-
         case STIVALE2_MMAP_ACPI_NVS:
             return "ACPI Non Volatile Storage";
-
         case STIVALE2_MMAP_BAD_MEMORY:
             return "Bad Memory";
-
         case STIVALE2_MMAP_BOOTLOADER_RECLAIMABLE:
             return "Bootloader Reclaimable";
-
         case STIVALE2_MMAP_KERNEL_AND_MODULES:
             return "Kernel And Modules";
-
         case STIVALE2_MMAP_FRAMEBUFFER:
             return "Framebuffer";
-
         default:
             return "Unknown";
     }
 }
 
-void *pmm_find_first_free_page(size_t page_count)
+// search bitmap for contiguous unused bits -> free pages
+void *pmm_find_first_free_page_range(size_t page_count)
 {
-    // can't find in no memory
-    if (page_count == 0)
-        return NULL;
-
-    for (size_t counter = 0; counter < page_count; counter++)
+    for (size_t all_bits_i = 0; all_bits_i < PAGE_TO_BIT(highest_page_top); all_bits_i++)
     {
-        for (size_t i = 0; i < PAGE_TO_BIT(highest_page); i++)
-        {
-            if (!bitmap_check_bit(&bitmap, i))
-                return (void *)BIT_TO_PAGE(i);
-        }
+	for (size_t page_count_i = 0; page_count_i < page_count; page_count_i++)
+	{
+	    if (bitmap_check_bit(&pmm_bitmap, all_bits_i + PAGE_TO_BIT(page_count_i)))
+		break;
+
+	    if (page_count_i == page_count - 1)
+		return (void *)BIT_TO_PAGE(all_bits_i);
+	}
     }
 
-    // nothing big enough
     return NULL;
 }
