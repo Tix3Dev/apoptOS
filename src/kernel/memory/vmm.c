04e286e30d1ef7a1f80075819db763f548020ba7
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-05-04 22:36:58 +0200

    [new] push local changes (small vmm changes)

--- src/kernel/memory/vmm.c
+++ src/kernel/memory/vmm.c
@@ -52,7 +52,7 @@ void vmm_init(struct stivale2_struct *stivale2_struct)
     assert(root_page_table != NULL);
 
     vmm_map_range(root_page_table, 0, 4 * GB, HIGHER_HALF_DATA_LV4, KERNEL_READ_WRITE);
-    vmm_map_range(root_page_table, 0, 2 * GB, HIGHER_HALF_CODE, KERNEL_READ_WRITE);
+    vmm_map_range(root_page_table, 0, 2 * GB, HIGHER_HALF_CODE, KERNEL_READ);
     for (uint64_t i = 0; i < memory_map->entries; i++)
     {
         current_entry = &memory_map->memmap[i];
@@ -60,7 +60,9 @@ void vmm_init(struct stivale2_struct *stivale2_struct)
         vmm_map_range(root_page_table, 0, current_entry->length, HIGHER_HALF_DATA_LV4, KERNEL_READ_WRITE);
     }
 
+    log(INFO, "Replaced bootloader page table at 0x%.16llx\n", asm_read_cr(3));
     vmm_load_page_table(root_page_table);
+    log(INFO, "Now using kernel page table at 0x%.16llx\n", asm_read_cr(3));
 
     log(INFO, "VMM initialized\n");
 }
