142d264e2ec3854104f5360a50d3f76fafa445f9
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-06-05 22:33:00 +0200

    [new] logs in malloc

--- src/kernel/libk/malloc/malloc.c
+++ src/kernel/libk/malloc/malloc.c
@@ -27,6 +27,7 @@
 
 #include <boot/stivale2.h>
 #include <libk/malloc/malloc.h>
+#include <libk/serial/log.h>
 #include <memory/dynamic/slab.h>
 #include <memory/physical/pmm.h>
 #include <memory/mem.h>
@@ -50,6 +51,9 @@ void malloc_heap_init(void)
     slab_caches[5] = slab_cache_create("heap slab size 128", 128, SLAB_PANIC | SLAB_AUTO_GROW);
     slab_caches[6] = slab_cache_create("heap slab size 256", 256, SLAB_PANIC | SLAB_AUTO_GROW);
     slab_caches[7] = slab_cache_create("heap slab size 512", 512, SLAB_PANIC | SLAB_AUTO_GROW);
+
+    log(INFO, "Slab caches for heap initialized\n");
+    log(INFO, "Heap fully initialized\n");
 }
 
 // allocate memory depending on the size, store metadata for free()
