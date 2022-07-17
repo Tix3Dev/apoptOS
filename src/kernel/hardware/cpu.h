9d84f161792526e6f0f2a296fc5b3b861e644ac7
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-07-17 11:33:22 +0200

    [new] cpuid implementation

--- src/kernel/hardware/cpu.h
+++ src/kernel/hardware/cpu.h
@@ -16,10 +16,21 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+/*
+    cpuid() is a modified version of this implementation:
+	https://github.com/qword-os/qword/blob/2e7899093d597dc55d8ed0d101f5c0509293d62f/src/sys/cpu.h#L67-L79
+
+    Under the following license:
+	https://github.com/qword-os/qword/blob/master/LICENSE
+*/
+
 #ifndef CPU_H
 #define CPU_H
 
 #include <stdint.h>
+
+#include <libk/malloc/malloc.h>
+#include <libk/printf/printf.h>
 
 typedef struct
 {
@@ -48,5 +59,131 @@ typedef struct
     uint64_t rsp;
     uint64_t ss;
 } interrupt_cpu_state_t;
+
+typedef struct
+{
+    uint32_t leaf;
+    uint32_t subleaf;
+
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+} cpuid_registers_t;
+
+enum
+{
+    CPUID_GET_VENDOR_STRING,
+    CPUID_GET_FEATURES
+};
+
+enum
+{
+    CPUID_FEAT_ECX_SSE3		= 1 << 0, 
+    CPUID_FEAT_ECX_PCLMUL	= 1 << 1,
+    CPUID_FEAT_ECX_DTES64	= 1 << 2,
+    CPUID_FEAT_ECX_MONITOR	= 1 << 3,  
+    CPUID_FEAT_ECX_DS_CPL	= 1 << 4,  
+    CPUID_FEAT_ECX_VMX		= 1 << 5,  
+    CPUID_FEAT_ECX_SMX		= 1 << 6,  
+    CPUID_FEAT_ECX_EST		= 1 << 7,  
+    CPUID_FEAT_ECX_TM2		= 1 << 8,  
+    CPUID_FEAT_ECX_SSSE3	= 1 << 9,  
+    CPUID_FEAT_ECX_CID		= 1 << 10,
+    CPUID_FEAT_ECX_SDBG		= 1 << 11,
+    CPUID_FEAT_ECX_FMA		= 1 << 12,
+    CPUID_FEAT_ECX_CX16		= 1 << 13, 
+    CPUID_FEAT_ECX_XTPR		= 1 << 14, 
+    CPUID_FEAT_ECX_PDCM		= 1 << 15, 
+    CPUID_FEAT_ECX_PCID		= 1 << 17, 
+    CPUID_FEAT_ECX_DCA		= 1 << 18, 
+    CPUID_FEAT_ECX_SSE4_1	= 1 << 19, 
+    CPUID_FEAT_ECX_SSE4_2	= 1 << 20, 
+    CPUID_FEAT_ECX_X2APIC	= 1 << 21, 
+    CPUID_FEAT_ECX_MOVBE	= 1 << 22, 
+    CPUID_FEAT_ECX_POPCNT	= 1 << 23, 
+    CPUID_FEAT_ECX_TSC		= 1 << 24, 
+    CPUID_FEAT_ECX_AES		= 1 << 25, 
+    CPUID_FEAT_ECX_XSAVE	= 1 << 26, 
+    CPUID_FEAT_ECX_OSXSAVE	= 1 << 27, 
+    CPUID_FEAT_ECX_AVX		= 1 << 28,
+    CPUID_FEAT_ECX_F16C		= 1 << 29,
+    CPUID_FEAT_ECX_RDRAND	= 1 << 30,
+    CPUID_FEAT_ECX_HYPERVISOR	= 1 << 31,
+    
+    CPUID_FEAT_EDX_FPU		= 1 << 0,  
+    CPUID_FEAT_EDX_VME		= 1 << 1,  
+    CPUID_FEAT_EDX_DE		= 1 << 2,  
+    CPUID_FEAT_EDX_PSE		= 1 << 3,  
+    CPUID_FEAT_EDX_TSC		= 1 << 4,  
+    CPUID_FEAT_EDX_MSR		= 1 << 5,  
+    CPUID_FEAT_EDX_PAE		= 1 << 6,  
+    CPUID_FEAT_EDX_MCE		= 1 << 7,  
+    CPUID_FEAT_EDX_CX8		= 1 << 8,  
+    CPUID_FEAT_EDX_APIC		= 1 << 9,  
+    CPUID_FEAT_EDX_SEP	    	= 1 << 11, 
+    CPUID_FEAT_EDX_MTRR	    	= 1 << 12, 
+    CPUID_FEAT_EDX_PGE	    	= 1 << 13, 
+    CPUID_FEAT_EDX_MCA	    	= 1 << 14, 
+    CPUID_FEAT_EDX_CMO	    	= 1 << 15, 
+    CPUID_FEAT_EDX_PAT	    	= 1 << 16, 
+    CPUID_FEAT_EDX_PSE36	= 1 << 17, 
+    CPUID_FEAT_EDX_PSN		= 1 << 18, 
+    CPUID_FEAT_EDX_CLFLUSH	= 1 << 19, 
+    CPUID_FEAT_EDX_DS		= 1 << 21, 
+    CPUID_FEAT_EDX_ACPI		= 1 << 22, 
+    CPUID_FEAT_EDX_MMX		= 1 << 23, 
+    CPUID_FEAT_EDX_FXSR		= 1 << 24, 
+    CPUID_FEAT_EDX_SSE		= 1 << 25, 
+    CPUID_FEAT_EDX_SSE2		= 1 << 26, 
+    CPUID_FEAT_EDX_SS		= 1 << 27, 
+    CPUID_FEAT_EDX_HTT		= 1 << 28, 
+    CPUID_FEAT_EDX_TM		= 1 << 29, 
+    CPUID_FEAT_EDX_IA64		= 1 << 30,
+    CPUID_FEAT_EDX_PBE		= 1 << 31
+};
+
+// retrieve information about cpu through cpuid instruction
+static inline int cpuid(cpuid_registers_t *registers)
+{
+    uint32_t cpuid_max;
+
+    asm volatile("cpuid"
+	    : "=a" (cpuid_max)
+	    : "a" (registers->leaf & 0x80000000)
+	    : "rbx", "rcx", "rdx");
+
+    if (registers->leaf > cpuid_max)
+	return 0;
+
+    asm volatile("cpuid"
+	    : "=a" (registers->eax), "=b" (registers->ebx), "=c" (registers->ecx), "=d" (registers->edx)
+	    : "a" (registers->leaf), "c" (registers->subleaf));
+
+    return 1;
+}
+
+// get cpu vendor id string by using cpuid
+static inline char *cpu_get_vendor_id_string(void)
+{
+    cpuid_registers_t *regs = &(cpuid_registers_t)
+    {
+        .leaf = CPUID_GET_VENDOR_STRING,
+        .subleaf = 0,
+
+        .eax = 0,
+        .ebx = 0,
+        .ecx = 0,
+        .edx = 0
+    };
+
+    cpuid(regs);
+
+    char *vendor_string = malloc(13);
+    snprintf(vendor_string, 13, "%.4s%.4s%.4s",
+	    (char *)&regs->ebx, (char *)&regs->edx, (char *)&regs->ecx);
+
+    return vendor_string;
+}
 
 #endif
