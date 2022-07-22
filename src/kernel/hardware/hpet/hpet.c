9231ba36866cc39ae96629c67f01d44595f9c3bf
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-07-23 00:48:00 +0200

    [new] HPET implementation

--- src/kernel/hardware/hpet/hpet.c
+++ src/kernel/hardware/hpet/hpet.c
@@ -19,20 +19,46 @@
 /*
 
     Brief file description:
-    blah
+    Provides a sleep function and function to get the counter to measure time between events
+    using the HPET.
 
 */
 
+#include <boot/stivale2.h>
 #include <hardware/hpet/hpet.h>
+#include <hardware/acpi/acpi.h>
+#include <memory/mem.h>
 
-/* utility function prototypes */
-
-//
+static volatile hpet_t *hpet;
+static volatile hpet_regs_t *hpet_regs;
 
 /* core functions */
 
-//
+// get HPET and set necessary variables
+void hpet_init(void)
+{
+    hpet = (hpet_t *)(uintptr_t)acpi_find_sdt("HPET");
+    hpet_regs = (hpet_regs_t *)(PHYS_TO_HIGHER_HALF_DATA(hpet->address));
 
-/* utility functions */
+    hpet_regs->counter_value = 0;
+    hpet_regs->general_config = 1;
+}
 
-//
+// wait for us nanoseconds
+void hpet_usleep(size_t us)
+{
+    uint32_t clock_period = hpet_regs->capabilities >> 32;
+
+    volatile size_t target_value = hpet_regs->counter_value + (us * (1000000000 / clock_period));
+
+    while (hpet_regs->counter_value < target_value)
+    {
+	asm volatile("pause");
+    }
+}
+
+// return the current counter value
+uint64_t hpet_get_counter_value(void)
+{
+    return hpet_regs->counter_value;
+}
--- src/kernel/hardware/hpet/hpet.h
+++ src/kernel/hardware/hpet/hpet.h
@@ -19,8 +19,42 @@
 #ifndef HPET_H
 #define HPET_H
 
-// datatypes / variables
+#include <stddef.h>
+#include <stdint.h>
 
-// core functions
+#include <hardware/acpi/tables/sdt.h>
+
+typedef struct __attribute__((__packed__))
+{
+    sdt_t header;
+    uint8_t hardware_rev_id;
+    uint8_t info; 
+    uint16_t pci_id;
+    uint8_t address_space_id;
+    uint8_t register_width;
+    uint8_t register_offset;
+    uint8_t reserved;
+    uint64_t address;
+    uint8_t hpet_num;
+    uint16_t minimum_ticks;
+    uint8_t page_protection;
+} hpet_t;
+
+typedef struct __attribute__((packed))
+{
+    uint64_t capabilities;
+    uint64_t reserved0;
+    uint64_t general_config;
+    uint64_t reserved1;
+    uint64_t int_status;
+    uint64_t reserved2;
+    uint64_t reserved3[24];
+    volatile uint64_t counter_value;
+    uint64_t unused4;
+} hpet_regs_t;
+
+void hpet_init(void);
+void hpet_usleep(size_t us);
+uint64_t hpet_get_counter_value(void);
 
 #endif
