855272d154241c5bbda25c3c2e500d950ae26f9f
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-08-06 02:31:07 +0200

    [refactor] improved sketch of smp rewrite

--- src/kernel/proc/smp/smp.c
+++ src/kernel/proc/smp/smp.c
@@ -36,7 +36,7 @@
 #include <libk/lock/spinlock.h>
 #include <libk/malloc/malloc.h>
 #include <libk/serial/log.h>
-// #include <libk/testing/assert.h>
+#include <libk/testing/assert.h>
 #include <memory/physical/pmm.h>
 #include <memory/mem.h>
 #include <proc/smp/smp.h>
@@ -45,13 +45,12 @@ static spinlock_t smp_lock;
 
 cpu_local_t *cpu_locals;
 static uint32_t cpus_online = 0;
-static uint32_t bsp_lapic_id = 0;
 
 /* utility function prototypes */
 
-static void bsp_init(struct stivale2_smp_info *smp_info);
-static void ap_init(struct stivale2_smp_info *smp_info);
-static void generic_cpu_local_init(void);
+static void bsp_init(struct stivale2_smp_info *smp_entry);
+static void ap_init(struct stivale2_smp_info *smp_entry);
+static void generic_cpu_local_init(uint64_t cpu_number, uint32_t lapic_id);
 
 /* core functions */
 
@@ -63,32 +62,25 @@ void smp_init(struct stivale2_struct *stivale2_struct)
     log(INFO, "Total CPU count: %d\n", smp_tag->cpu_count);
 
     cpu_locals = malloc(smp_tag->cpu_count * sizeof(cpu_local_t));
-    bsp_lapic_id = smp_tag->bsp_lapic_id;
 
-    for (size_t i = 0; i < smp_tag->cpu_count; i++)
+    for (uint64_t i = 0; i < smp_tag->cpu_count; i++)
     {
 	spinlock_acquire(&smp_lock);
 
-	if (smp_tag->smp_info[i].lapic_id == bsp_lapic_id)
+	smp_tag->smp_info[i].extra_argument = i;
+
+	if (smp_tag->smp_info[i].lapic_id == smp_tag->bsp_lapic_id)
 	{
 	    bsp_init((void *)&smp_tag->smp_info[i]);
 	    
 	    continue;
 	}
 
-	cpu_locals[i].cpu_number = i;
-	cpu_locals[i].lapic_id = smp_tag->smp_info[i].lapic_id;
-	spinlock_acquire(&cpu_locals[i].exec_lock);
-	spinlock_release(&cpu_locals[i].exec_lock);
-
-	smp_tag->smp_info[i].extra_argument = (uint64_t)&cpu_locals[i];
-	
 	uint64_t stack = (uintptr_t)pmm_allocz(CPU_LOCALS_STACK_SIZE / PAGE_SIZE);
 	assert(stack != 0);
 	stack = PHYS_TO_HIGHER_HALF_DATA(stack + CPU_LOCALS_STACK_SIZE);
 
 	smp_tag->smp_info[i].target_stack = stack;
-
 	smp_tag->smp_info[i].goto_address = (uint64_t)ap_init;
 
 	spinlock_release(&smp_lock);
@@ -104,17 +96,40 @@ void smp_init(struct stivale2_struct *stivale2_struct)
 
 /* utility functions */
 
-static void bsp_init(struct stivale2_smp_info *smp_info)
+static void bsp_init(struct stivale2_smp_info *smp_entry)
 {
-    //
+    generic_cpu_local_init(smp_entry->extra_argument, smp_entry->lapic_id);
+
+    cpus_online++;
+
+    log(INFO, "CPU No. %ld: BSP fully initialized\n", smp_entry->extra_argument);
 }
 
-static void ap_init(struct stivale2_smp_info *smp_info)
+static void ap_init(struct stivale2_smp_info *smp_entry)
 {
-    //
+    spinlock_acquire(&smp_lock);
+
+    // load_gdt
+    // load_idt
+    generic_cpu_local_init(smp_entry->extra_argument, smp_entry->lapic_id);
+    // asm ("sti")
+
+    cpus_online++;
+
+    log(INFO, "CPU No. %ld: AP fully initialized\n", smp_entry->extra_argument);
+
+    for (;;)
+    {
+        asm volatile("hlt");
+    }
+
+    spinlock_release(&smp_lock);
 }
 
-static void generic_cpu_local_init(void)
+static void generic_cpu_local_init(uint64_t cpu_num, uint32_t lapic_id)
 {
-    //
+    cpu_locals[cpu_num].cpu_number = cpu_num;
+    cpu_locals[cpu_num].lapic_id = lapic_id;
+    spinlock_acquire(&cpu_locals[cpu_num].exec_lock);
+    spinlock_release(&cpu_locals[cpu_num].exec_lock);
 }
