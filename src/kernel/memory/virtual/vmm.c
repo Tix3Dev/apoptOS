18a54cad1d4e0cba0b53bbc3f1a325a4d9d810dd
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-08-13 20:34:59 +0200

    [fix] draft solution to PAT problem

--- src/kernel/memory/virtual/vmm.c
+++ src/kernel/memory/virtual/vmm.c
@@ -42,6 +42,17 @@ uint64_t *vmm_get_or_create_pml(uint64_t *pml, size_t pml_index, uint64_t flags)
 void vmm_set_pt_value(uint64_t *page_table, uint64_t virt_page, uint64_t flags, uint64_t value);
 void vmm_flush_tlb(void *address);
 
+
+
+
+
+
+void vmm_map_range_test(uint64_t *page_table, uint64_t start, uint64_t end, uint64_t offset, uint64_t flags);
+
+
+
+
+
 /* core functions */
 
 // create root page table and map important physical memory regions
@@ -87,17 +98,16 @@ void vmm_init(struct stivale2_struct *stivale2_struct)
     vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFB00000000000, KERNEL_READ_WRITE |
 	    vmm_pat_cache_to_flags(PAT_WRITE_COMBINING));
 
-    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFC00000000000, KERNEL_READ_WRITE |
-	    vmm_pat_cache_to_flags(PAT_WRITE_THROUGH));
+    vmm_map_range_test(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFC00000000000, KERNEL_READ_WRITE);
 
-    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFD00000000000, KERNEL_READ_WRITE |
-	    vmm_pat_cache_to_flags(PAT_WRITE_PROTECTED));
-
-    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFE00000000000, KERNEL_READ_WRITE |
-	    vmm_pat_cache_to_flags(PAT_WRITE_BACK));
-
-    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFF00000000000, KERNEL_READ_WRITE |
-	    vmm_pat_cache_to_flags(PAT_UNCACHED));
+//    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFD00000000000, KERNEL_READ_WRITE |
+//	    vmm_pat_cache_to_flags(PAT_WRITE_PROTECTED));
+//
+//    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFE00000000000, KERNEL_READ_WRITE |
+//	    vmm_pat_cache_to_flags(PAT_WRITE_BACK));
+//
+//    vmm_map_range(root_page_table, 0, HEAP_MAX_SIZE, 0xFFFFF00000000000, KERNEL_READ_WRITE |
+//	    vmm_pat_cache_to_flags(PAT_UNCACHED));
 
 
 
@@ -129,6 +139,63 @@ void vmm_map_range(uint64_t *page_table, uint64_t start, uint64_t end, uint64_t 
         vmm_map_page(page_table, i, i + offset, flags);
     }
 }
+
+
+
+
+
+
+
+
+// set a value in a page table entry and flush translation lookaside buffer
+void vmm_set_pt_value_test(uint64_t *page_table, uint64_t virt_page, uint64_t flags, uint64_t value)
+{
+    // index for page mapping level 4
+    size_t pml4_index	= (virt_page & ((uintptr_t)0x1ff << 39)) >> 39;
+    // index for page directory table
+    size_t pdpt_index	= (virt_page & ((uintptr_t)0x1ff << 30)) >> 30;
+    // index for page directory
+    size_t pd_index	= (virt_page & ((uintptr_t)0x1ff << 21)) >> 21;
+    // index for page table
+    size_t pt_index	= (virt_page & ((uintptr_t)0x1ff << 12)) >> 12;
+
+    // page mapping level 4 = pml4
+    uint64_t *pml4  = page_table;
+    // page directory table = pml3
+    uint64_t *pdpt  = vmm_get_or_create_pml(pml4, pml4_index, flags);
+    // page directory	    = pml2
+    uint64_t *pd    = vmm_get_or_create_pml(pdpt, pdpt_index, flags);
+    // page table	    = pml1
+    uint64_t *pt    = vmm_get_or_create_pml(pd, pd_index, flags);
+
+    // actual mapped value (either physical frame address or 0)
+    pt[pt_index]    = value | vmm_pat_cache_to_flags(PAT_WRITE_THROUGH);
+
+    // for changes to apply, the translation lookaside buffers need to be flushed
+    vmm_flush_tlb((void *)virt_page);
+}
+
+// set a page table entry for a new virtual memory address, which will be mapped to a physical frame
+void vmm_map_page_test(uint64_t *page_table, uint64_t phys_page, uint64_t virt_page, uint64_t flags)
+{
+    uint64_t pt_value = phys_page | flags;
+    vmm_set_pt_value_test(page_table, ALIGN_DOWN(virt_page, PAGE_SIZE), flags, pt_value);
+}
+
+// map a whole physical memory region with custom offset
+void vmm_map_range_test(uint64_t *page_table, uint64_t start, uint64_t end, uint64_t offset, uint64_t flags)
+{
+    for (uint64_t i = ALIGN_DOWN(start, PAGE_SIZE); i < ALIGN_UP(end, PAGE_SIZE); i += PAGE_SIZE)
+    {
+        vmm_map_page_test(page_table, i, i + offset, flags);
+    }
+}
+
+
+
+
+
+
 
 // unmap a whole physical memory region
 void vmm_unmap_range(uint64_t *page_table, uint64_t start, uint64_t end)
